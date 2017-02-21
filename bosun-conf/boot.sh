d4391ecb2fe3716c4a536977a40cd90ec76ab89b
HYmian <gopher.mian@outlook.com>
2017-02-21 15:51:10 +0800

    update bosun-conf boot.sh

--- bosun-conf/boot.sh
+++ bosun-conf/boot.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
-# service=`curl http://rancher-metadata/latest/self/container/service_name`
-id=`curl http://rancher-metadata/latest/self/container/service_index`
-# uuid=`curl http://rancher-metadata/latest/self/container/uuid`
-ip=`curl http://rancher-metadata/latest/self/container/ips/0`
-name=`curl http://rancher-metadata/latest/self/container/name`
-# port=`curl http://rancher-metadata/latest/self/container/ports/0`
+# service=`wget -qO- http://rancher-metadata/latest/self/container/service_name`
+id=`wget -qO- http://rancher-metadata/latest/self/container/service_index`
+# uuid=`wget -qO- http://rancher-metadata/latest/self/container/uuid`
+ip=`wget -qO- http://rancher-metadata/latest/self/container/ips/0`
+name=`wget -qO- http://rancher-metadata/latest/self/container/name`
+# port=`wget -qO- http://rancher-metadata/latest/self/container/ports/0`
 
-bin=`dirname "${BASH_SOURCE-$0}"`
-bin=`cd "$bin"; pwd`
+# bin=`dirname "${BASH_SOURCE-$0}"`
+# bin=`cd "$bin"; pwd`
 
-cat ./request.tmpl | sed \
+cat /data/request.tmpl | sed \
     -e "s/@ID@/${id}/g" \
     -e "s/@Name@/${name}/g" \
     -e "s/@IP@/${ip}/g"
