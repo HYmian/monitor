67e2c81cd3d0214d2563cf1e18eead1f9ffa3e7f
Markus Langer <langer.markus@gmail.com>
2017-04-24 13:36:24 +0200

    added autocommit script

---
+++ bosun-conf/commit.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#echo "First arg: $1"
+#echo "Second arg: $2"
+#echo "third arg: $3"
+
+#echo $1 >> /tmp/args.txt
+#echo $2 >> /tmp/args.txt
+#echo $3 >> /tmp/args.txt
+
+git commit -am "$2 : $3"
